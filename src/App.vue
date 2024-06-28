ef7fed50825726d2591ec52f5238d67b3062c415
zhaixuwen <zhaixuwen@163.com>
2024-06-28 07:37:21 +0000

    Upload app.vue and main.js

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import HomePage from '@/components/HomePage.vue'
+</script>
+
+<template>
+  <div id="app">
+    <HomePage />
+  </div>
+</template>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+}
+</style>
