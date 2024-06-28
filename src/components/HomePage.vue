901bff31d1b3ba2680e92a18b81989a39f73b66d
zhaixuwen <zhaixuwen@163.com>
2024-06-28 07:30:33 +0000

    Upload project again

---
+++ src/components/HomePage.vue
@@ -0,0 +1,44 @@
+<script setup>
+import UUIDCard from '@/components/UUIDCard.vue'
+
+</script>
+
+<template>
+  <div class="home-page">
+    <el-container>
+      <el-header class="home-header">
+        <el-menu mode="horizontal">
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <el-space>
+            <img src="../assets/logo.svg" alt="Toolkit Logo" height="24px" width="24px" />
+            <h1>Testing Toolkit</h1>
+          </el-space>
+        </el-menu>
+      </el-header>
+      <el-main class="home-main">
+        <el-row :gutter="20">
+          <el-col :span="6">
+            <UUIDCard />
+          </el-col>
+          <el-col :span="6">
+            <div class="grid-content ep-bg-purple" />
+          </el-col>
+          <el-col :span="6">
+            <div class="grid-content ep-bg-purple" />
+          </el-col>
+          <el-col :span="6">
+            <div class="grid-content ep-bg-purple" />
+          </el-col>
+        </el-row>
+      </el-main>
+    </el-container>
+  </div>
+</template>
+
+<style scoped>
+.home-header {
+  margin: 0;
+  padding: 0;
+}
+
+</style>
---
+++ src/components/UUIDCard.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { ref } from 'vue'
+
+const smallUUID = ref(crypto.randomUUID())
+const bigUUID = ref(smallUUID.value.toUpperCase())
+
+const generateUUID = () => {
+  smallUUID.value = crypto.randomUUID()
+  bigUUID.value = smallUUID.value.toUpperCase()
+}
+
+</script>
+
+<template>
+  <div>
+    <el-card shadow="hover">
+      <template #header>
+        <div class="uuid-header">
+          <span>UUID(v4) Generator</span>
+        </div>
+      </template>
+      <div class="uuid-content">
+        <el-row :gutter="10">
+          <el-col :span="18">
+            <el-input v-model="smallUUID" style="max-width: 330px;" placeholder="Click button to generate" />
+          </el-col>
+          <el-col :span="6">
+            <el-button type="primary" @click="generateUUID()" color="#FFB243">Generate</el-button>
+          </el-col>
+        </el-row>
+        <el-row :gutter="10">
+          <el-col :span="18">
+            <el-input v-model="bigUUID" style="max-width: 330px;" placeholder="Click button to generate" />
+          </el-col>
+          <el-col :span="6"></el-col>
+        </el-row>
+      </div>
+    </el-card>
+  </div>
+
+</template>
+
+<style scoped>
+.el-row {
+  margin-bottom: 10px;
+}
+</style>
